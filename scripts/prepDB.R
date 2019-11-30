eee3985f27cff4c1e8133bf29dba9cf794740c8d
Gerard <gerard.ros@nmi-agro.nl>
2019-11-30 17:19:37 +0100

    update prepDB kp / nomogram

--- scripts/prepDB.R
+++ scripts/prepDB.R
@@ -1,6 +1,9 @@
 # prepare final database
 # Laura Moria & Gerard H. Ros, november-19
 
+  # require packages
+  require(data.table);require(sf);require(dplyr)
+
   # inladen basis bestanden ----
   
   # water types
@@ -23,6 +26,9 @@
     hybi[limietsymbool == '<', meetwaarde := meetwaarde * 0.5]
     hybi[fewsparameter == 'WATDTE_m' & jaar == 2006 & planvanaanpak == 'PVA MAFY LIJN 2006 WP', meetwaarde := meetwaarde]
     
+    # filter measurements on periode since 2000
+    hybi <- hybi[jaar>=2000]
+    
     # merge with locaties
     hybi <- merge(hybi,locaties[,c('CODE','EAGIDENT')],by.x ='locatiecode', by.y = 'CODE')
     hybi <- merge(hybi,eag_wl[,c('watertype','GAFIDENT')],by.x ='EAGIDENT', by.y = 'GAFIDENT')
@@ -42,10 +48,6 @@
     EKRset2 <- readRDS('hydrobiologie/EKRset_OvWater.rds') %>% as.data.table()
     EKRset <- rbind(EKRset1,EKRset2,fill=T)
     
-    # add coordinates
-    EKRset[,XCOORD := as.integer(XCOORD)]
-    EKRset[,YCOORD := as.integer(YCOORD)]
-    
     # remove sets
     rm(EKRset1,EKRset2)
     
@@ -56,6 +58,24 @@
   # inladen KRW doelen 
   doelen <- fread('hydrobiologie/doelen.csv')
   
+  # inladen waterbalans data (made by loadBalances)
+  dat <- readRDS("pbelasting/dat.rds") 
   
+  # load datafile with P-load PC Ditch-----------
+  
+    # load datafile
+    Overzicht_kP <- fread('data/Overzicht_kP.csv') 
+    
+    # rename columns
+    setnames(Overzicht_kP,c('EAG','GAF','EAGnaam','plv_o2','plv','opp','diepte','fr_moeras','strijklengte',
+                            'debiet','inflow','extinctie','sedimenttype','pgrens_helder_troebel',
+                            'pgrens_troebel_helder','type','morfologie','systeemgrens','p_load'))
+  
+    # remove columns
+    cols <- c('opp','EAGnaam')
+    Overzicht_kP[,c(cols):=NULL]
+    
+    # load nonogram
+    nomogram <- fread('data/nomogram.csv')
   
     
