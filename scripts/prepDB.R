7d2b6825de54286d6b5731434d3537cc27b4447a
Moria <laura.moria@waternet.nl>
2020-01-10 13:06:31 +0100

    adapt prepDB

--- scripts/prepDB.R
+++ scripts/prepDB.R
@@ -1,135 +1,176 @@
 # prepare final database
 # Laura Moria & Gerard H. Ros, november-19
 
+  # clear environment
+  rm(list=ls())
+  
   # require packages
   require(data.table);require(sf);require(dplyr)
 
+  # source functions
+  source('scripts/ppr_funs.R')
+  
   # inladen basis bestanden ----
   
-  # water types
-  watertypen <- fread('data/KRWWatertype.csv')
+    # water types
+    watertypen <- fread('data/KRWWatertype.csv')
+    
+    # locaties van alle metingen (water, biologie, en slootbodem)
+    locaties <- fread('data/Location.csv')
+    
+    # locaties van EAG oppervlaktes
+    eag_wl <- fread('data/Oppervlaktes EAGs en Water.csv')
   
-  # locaties van alle metingen (water, biologie, en slootbodem)
-  locaties <- fread('data/Location.csv')
-  
-  # locaties van EAG oppervlaktes
-  eag_wl <- fread('data/Oppervlaktes EAGs en Water.csv')
+    # shape with EAG
+    gEAG <- st_read('data/EAG20180612.gpkg',quiet = TRUE)
 
-  # inladen hydrobiologische gegevens ------
-  
-    # inladen gegevens hydrobiologie
+    # KRW doelen 
+    doelen <- fread('hydrobiologie/doelen.csv')
+
+    # nonogram
+    nomogram <- fread('data/nomogram.csv')
+    
+    # waterbalans data (made by loadBalances)
+    dat <- readRDS("pbelasting/dat.rds") 
+    
+    # gegevens hydrobiologie
     hybi <- readRDS('data/alles_reliable.rds')
     
-    # adapt few properties
-    hybi[, datum := as.Date(datum, format = "%Y-%m-%d %H:%M")]
-    hybi[, jaar := year(datum)]
-    hybi[limietsymbool == '<', meetwaarde := meetwaarde * 0.5]
-    hybi[fewsparameter == 'WATDTE_m' & jaar == 2006 & planvanaanpak == 'PVA MAFY LIJN 2006 WP', meetwaarde := meetwaarde]
-    
-    # filter measurements on periode since 2000
-    hybi <- hybi[jaar>=2000]
-    
-    # merge with locaties
-    hybi <- merge(hybi,locaties[,c('CODE','EAGIDENT')],by.x ='locatiecode', by.y = 'CODE')
-    hybi <- merge(hybi,eag_wl[,c('watertype','GAFIDENT')],by.x ='EAGIDENT', by.y = 'GAFIDENT')
-    
-    # add codes (is this really needed?)
-    hybi[,locatie.EAG := EAGIDENT]
-    hybi[,locatie.KRW.watertype := watertype]
-  
-    # remove columns with no data
-    cols <- colnames(hybi)[unlist(hybi[,lapply(.SD,function(x) sum(is.na(x))==nrow(hybi))])]
-    hybi[,c(cols) := NULL]
-    
-   # inladen EKR sets KRW en overig water ------  
-  
-    # inladen EKR sets KRW en overig water
+    # EKR sets KRW en overig water
     EKRset1 <- readRDS('hydrobiologie/EKRset_KRW.rds') %>% as.data.table()
     EKRset2 <- readRDS('hydrobiologie/EKRset_OvWater.rds') %>% as.data.table()
-    EKRset <- rbind(EKRset1,EKRset2,fill=T)
     
-    # remove sets
-    rm(EKRset1,EKRset2)
-    
-    # remove columns without information
-    cols <- colnames(EKRset)[unlist(EKRset[,lapply(.SD,function(x) sum(is.na(x))==nrow(EKRset))])]
-    EKRset[,c(cols):= NULL]
-  
-  # inladen KRW doelen 
-  doelen <- fread('hydrobiologie/doelen.csv')
-  
-  # inladen waterbalans data (made by loadBalances)
-  dat <- readRDS("pbelasting/dat.rds") 
-  
-  # load datafile with P-load PC Ditch-----------
-  
-    # load datafile
-    Overzicht_kP <- fread('data/Overzicht_kP.csv') 
-    
-    # rename columns
-    setnames(Overzicht_kP,c('EAG','GAF','EAGnaam','plv_o2','plv','opp','diepte','fr_moeras','strijklengte',
-                            'debiet','inflow','extinctie','sedimenttype','pgrens_helder_troebel',
-                            'pgrens_troebel_helder','type','morfologie','systeemgrens','p_load'))
-  
-    # remove columns
-    cols <- c('opp','EAGnaam')
-    Overzicht_kP[,c(cols):=NULL]
-    
-    # load nonogram
-    nomogram <- fread('data/nomogram.csv')
-  
-  # add slootbodem data
-    
-    # load csv file with measurement data
+    # slootbodem measurements
     bod  <- fread("data/bodemfews.csv")
     
-    # adapt file
-    bod[,datum := as.Date(datum, format = "%Y-%m-%d %H:%M")]
-    bod[,jaar := year(datum)]
-    bod[limietsymbool == '<',meetwaarde := meetwaarde * 0.5] 
-    
-    # merge with GAFIDENT from eag_wl
-    bod <- merge(bod,eag_wl[,c('watertype','GAFIDENT')],by.x='locatie EAG',by.y = 'GAFIDENT',all = FALSE)
-    
-    # wijzig relevante namen van bodemfews database
-    cols <- colnames(bod)
-    setnames(bod,c('locatie EAG','locatiecode','locatie omschrijving','locatie x','locatie y','locatie z','fewsparameter','compartiment'),
-             c('loc.eag','loc.code','loc.oms','loc.x','loc.y','loc.z','parm.fews','parm.compartiment'))
-    
-    # select properties and dcast table
-    selb <- dcast(bod, loc.eag+loc.code+loc.oms+loc.x+loc.y+loc.z+datum+jaar ~ parm.fews+parm.compartiment, value.var = "meetwaarde", fun.aggregate = mean)
-    
-    # adapt P measurement into one class, and NA gets class 8
-    selb[,klasseP := cut(`Ptot_gP/kg_dg_BS`,breaks = c(0,0.5,1,1.5,2.5,5,10,1000),labels=1:7)]
-    selb[,klasseP := factor(klasseP,levels=1:8)]
-    selb[is.na(klasseP), klasseP := 8]
-    
-  # load water quality information
-  
-    # load file rds
+    # waterquality measurements
     wq  <- readRDS("data/ImportWQ.rds") %>% as.data.table()
     
-    # adapt wq database
-    wq[,jaar := year(datum)]
-    wq[,maand := month(datum)]
-    wq[limietsymbool == '<',meetwaarde := meetwaarde * 0.5]
+    # datafile with P-load PC Ditch
+    Overzicht_kP <- fread('data/Overzicht_kP.csv') 
     
-    # delete years before 2000
-    wq <- wq[jaar>1999,]
-    
-    # merge with locaties, remove older EAGIDENT with new one
-    wq[,EAGIDENT := NULL]
-    wq <- merge(wq,locaties[,c('CODE','EAGIDENT')], by.x ='locatiecode', by.y = 'CODE',all.x = T)
-    wq <- merge(wq,eag_wl[,c('watertype','GAFIDENT')], by.x ='EAGIDENT', by.y = 'GAFIDENT', all =FALSE)
-    
-  # simoni : toxiciteitsdata
-    
-    # inladen van database
+    # toxiciteitsdata simoni
     simoni <- readRDS('data/simoni.rds')
+    
+  # update, filter and clean up databases -----------
+   
+    # EKR measurements
+    EKRset <- ppr_ekr(ekr1 = EKRset1,ekr2 = EKRset2)
+ 
+    # hybi measurements
+    hybi <- ppr_hybi(db = hybi,syear = 2000,wtype = eag_wl,mlocs = locaties)
+    
+    # slootbodemdata
+    bod <- ppr_slootbodem(db = bod, wtype = eag_wl)
+    
+    # water quality
+    wq <-  ppr_wq(db = wq, syear = 2000, wtype = eag_wl, mlocs = locaties)
+      
+    # Pload and limits from PC Ditch
+    Overzicht_kP <- ppr_pcditch(db = Overzicht_kP)
+
+  # calculate means per EAG or meetpunt -----------
+    
+    # calculate mean EKR per EAG
+    krw <- calc_mean_EKR(db = EKRset, nyears = 3,pEAG = TRUE, pYEAR = FALSE, pSEASON = FALSE)
+
+    # give GEP and oordeel
+    krw[,c('GEP','oordeel') := eval_EKR(id,GHPR,EKR,doelen)]
+    
+    # rename GHPR in more readible (and less long names)
+    krw[,GPHRnew := renameGHPR(GHPR)]
+    krw[,wbmethode := renameWbmethode(wbmethode)]
+    
+    # remove all data rows without EAGIDENT
+    krw <- krw[!is.na(EAGIDENT)]
+    
+    
+    dat <- loadBalances_lm(dir_bal = dir_bal,kopTab = kopTab,sfile = FALSE)
+    
+  
+# make final matrix
+    
+     makeMatrix <- function(EKRset, bod, wq, hybi, dat){
+      
+      #laatste 3 meetjaren EKR scores ---------
+      
+        # remove some data rows
+        EKRinp <- EKRset[!Grootheid.code %in% c("AANTPVLME", "SOORTRDM")]
+      
+        # derive mean EKR for last three years
+        krw <- tabelPerWL3jaargemEAG(EKRset = EKRinp,gEAG = gEAG,doelen)
+      
+        # rename GHPR in more readible (and less long names)
+        krw[,GPHRnew := renameGHPR(GHPR)]
+        krw[,wbmethode := renameWbmethode(wbmethode)]
+        
+        # remove all data rows without EAGIDENT
+        krw <- krw[!is.na(EAGIDENT)]
+        
+      # dcst om maatlatten aprt te beschouwen en relaties tussen maatlatten te leggen
+      krw <- dcast(krw, EAGIDENT + geo_id + watertype ~ GPHRnew + wbmethode, 
+                    value.var = "EKR", fun.aggregate = mean)
+      
+      # koppel op jaar? nee want dat doet P belasting en bodem ook niet en dan zijn er te veel mismatches
+      krw[,GAF := substr(EAGIDENT, 1, 4)]
+      
+      # p vskp ---------------
+      PvskP <- makePmaps(dbwbal = dat, dbhybi = hybi,dbnomogram = nomogram,
+                         dbov_kP = Overzicht_kP, dbeag_wl = eag_wl)
+
+      # bodem toevoegen ------------------
+      gemwaterbod <- bodsam(bod,cmean = TRUE)
+      
+      # hybi indicatoren matrix maken obv mediaan per gebied en gemiddelde over jaren van laatste drie meetjaren ------
+      # compartiment slecteren en soms EZ en soms OW: nog niet gedaan
+      d4 <- calcMeanHybi(dbhybi = hybi)
+      
+      # add water quality data
+      wq <- copy(wq)
 
         
+     # this is for wq1
+      db <- db[fewsparametercategorie %in% srow & jaar > 2015]
+      # dcast, aantal unieke locatiecodes per EAG+jaar waar SUBMSPTN is gerapporteerd.)
+      db <- dcast(db,locatie.EAG ~ fewsparameter+fewsparameterparameterfractie+fewsparametereenheidequivalent+eenheid, 
+                  mean, value.var = c("meetwaarde"))
+      
+      # rename columns
+      # misschien zomerhalfjaargemiddelden toevoegen?
+      setnames(db,gsub("___","_",colnames(db)))
+      setnames(db,gsub("__","_",colnames(db)))
+      
+   # merge tot matrix -----
+      
+      matrix1 <- sp::merge(PvskP, krw, by.x = 'EAG', by.y = 'EAGIDENT', all.y = TRUE)
+      
+      matrix2 <-sp::merge(matrix1,gemwaterbod,by.x = 'EAG', by.y = 'locatie.EAG', all.x = TRUE)
+      
+      matrix4 <- sp::merge(matrix2, d4,by.x = 'EAG', by.y = 'locatie.EAG', all.x = TRUE)
+      
+      matrix5 <- sp::merge(matrix4,wq1, by.x = 'EAG', by.y = 'locatie.EAG', all.x = TRUE)
+      
+      saveRDS(matrix5,'../matrix/matrix.rds')
+      write.table(matrix5, file = paste("../matrix/matrix",format(Sys.time(),"%Y%m%d%H%M"),".csv", sep= ""), quote = FALSE, na = "", sep =';', row.names = FALSE)
+      return(matrix5)
+    }
     
-    
+
+     # part of tabelPerWL3jaargemEAG --- 
+     # add type water body
+     db[,wl := sapply(strsplit(id, '_'), `[`, 2)]
+     db[is.na(wl), wl := paste0('gewogen_',id)]
+     db[wl=='OvWa',wl := sapply(strsplit(id, '_'), `[`, 3)]
+     db[!is.na(EAGIDENT), wl := EAGIDENT]
+     
+     # merge with EAG shape
+     d3 <- merge(d2, gEAG[,c('GAFIDENT','GAFNAAM')], by.x = 'wl',by.y = 'GAFIDENT', all.x = TRUE)
+     
+     # setnames
+     setnames(d3,c('wl','id'),c('waterlichaam','geo_id'))
+     
+     # remove the geo component
+     d3[,geom := NULL]
     
     
     
